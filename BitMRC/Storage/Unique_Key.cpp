4a1783ba437e66b75e22166d7ac6e0cfbb1e9625
Gerhard Dutka <github@dutka-is.de>
2016-06-30 07:30:57 +0200

    WIP: UQK

--- BitMRC/Storage/Unique_Key.cpp
+++ BitMRC/Storage/Unique_Key.cpp
@@ -7,7 +7,13 @@
 #include <Addr.h>
 #include <Storage/Unique_Key.h>
 #include <string>
-
+#include <sha3.h>
+#include <ios>
+#ifdef DEBUG_UQK
+#define STORABLE_UQK(a) printf a;
+#else
+#define STORABLE_UQK(a)
+#endif
 Unique_Key::Unique_Key() {
 
 }
@@ -24,7 +30,12 @@ bool Unique_Key::operator==(Addr & addr_in) {
 }
 
 bool Unique_Key::set_key(std::string key_in) {
-	this->key = key_in;
+	unsigned char digest[24];
+	SHA3 sha(16);
+	sha.CalculateDigest(digest, key_in.c_str(), key_in.length());
+	this->key.assign(digest);
+	cout << ios::hex << digest ;
+//	STORABLE_UQK(("%s: KEY is %s\n", __func__, digest));
 	return true;
 }
 
